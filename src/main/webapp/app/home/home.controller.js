6e9caa96c237b04599540c16f11156daa5d4a94c
d-leroy <dorian.leroy@irisa.fr>
2017-05-12 15:06:36 +0200

    Added upvoting and downvoting functionality

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -40,5 +40,57 @@
                 vm.searchQuery = null;
             });
         }
+
+        function removeUser(array) {
+            if (array !== undefined) {
+                return array.filter(function (user) {
+                    return user.id !== vm.account.id;
+                });
+            }
+        }
+
+        function containsUser(array) {
+            return array !== undefined && array.filter(function (user) {
+                return user.id === vm.account.id;
+            }).length > 0
+        }
+
+        $scope.setupPost = function (post) {
+            if (post.score === undefined || post.score === null) {
+                post.score = 0;
+            }
+        }
+
+        $scope.isUpvoter = function (post) {
+            return containsUser(post.upvoters);
+        }
+
+        $scope.isDownvoter = function (post) {
+            return containsUser(post.downvoters);
+        }
+
+        $scope.upvote = function (post) {
+            if (containsUser(post.upvoters)) {
+                post.upvoters = removeUser(post.upvoters);
+                post.score = post.score - 1;
+            } else {
+                post.upvoters.push(vm.account);
+                post.downvoters = removeUser(post.downvoters);
+                post.score = post.score + 1;
+            }
+            Post.update(post);
+        }
+
+        $scope.downvote = function (post) {
+            if (containsUser(post.downvoters)) {
+                post.downvoters = removeUser(post.downvoters);
+                post.score = post.score + 1;
+            } else {
+                post.downvoters.push(vm.account);
+                post.upvoters = removeUser(post.upvoters);
+                post.score = post.score - 1;
+            }
+            Post.update(post);
+        }
     }
 })();
